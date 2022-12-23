c75ca28e3ab246dab457565ba9a89659911c4378
uankit22 <95609289+uankit22@users.noreply.github.com>
2022-12-23 20:00:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var img = "images/dice"+ randomNumber1 +".png";
+document.querySelector(".img1").setAttribute("src", img);
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var img = "images/dice"+ randomNumber2 +".png";
+document.querySelector(".img2").setAttribute("src", img);
+
+if(randomNumber1>randomNumber2){
+document.querySelector("h1").innerHTML = "Player 1 Wins"
+}
+else if(randomNumber1<randomNumber2){
+document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else{
+  document.querySelector("h1").innerHTML = "Oopps it's a Draw";
+
+}
